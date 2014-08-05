d3f7fcb70ff8b5633d84f9a1fd8b1a203cae070e
Charlie Hield <charles.hield@gmail.com>
2014-08-05 13:44:04 -0500

    first commit

---
+++ templates/project/manifest.rb
@@ -0,0 +1,22 @@
+# Description
+description "A Compass extension for making fluid videos."
+
+# Stylesheet Import
+file '_compass-fluid-video-demo.scss', :like => :stylesheet, :media => 'screen, projection'
+
+# Javascript Import
+# file 'scripts.js', :like => :javascript, :to => 'scripts.js'
+
+# General File Import
+# file 'README.md', :to => "README.md"
+
+# Compass Extension Help
+help %Q{
+  Open _compass-fluid-video-demo.scss for help and examples.
+}
+
+# Compass Extension Welcome Message
+#  Users will see this when they create a new project using this template.
+welcome_message %Q{
+  You have successfully installed compass-fluid-video!
+}
